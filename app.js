3de08ee2e5a6b887d428b3606ed0ef78a08b9873
Soundlover1984 <denys.kovtun@gmail.com>
2023-10-26 23:15:41 +0300

    swagger added

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 require("dotenv").config();
+
+
 
 // const usersRouter = require("./routes/api/users");
 // const authRouter = require("./routes/api/auth");
@@ -17,6 +21,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // app.use("api/users", usersRouter);
 // app.use("api/auth", authRouter);
 // app.use("/api/drinks", drinksRouter);
