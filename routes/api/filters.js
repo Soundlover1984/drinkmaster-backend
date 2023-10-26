7d91fd6831bff275cc2381f67c7b94e37461693d
Soundlover1984 <denys.kovtun@gmail.com>
2023-10-26 14:10:42 +0300

    filters logic added

---
+++ routes/api/filters.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const ctrl = require("../../controllers/filters");
+
+const { authenticate } = require("../../middlewares");
+
+const router = express.Router();
+
+router.get("/categories", authenticate, ctrl.getCategories);
+router.get("/ingredients", authenticate, ctrl.getIngredients);
+router.get("/glasses", authenticate, ctrl.getGlasses);
+
+module.exports = router;
